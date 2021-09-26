ed55035d1ec25fa2cbc13ab9b63b2476456de832
Temitope3665 <aroyewontope@gmail.com>
2021-09-26 16:29:08 +0100

    Updated Gravii-ng project

---
+++ script.js
@@ -0,0 +1,17 @@
+// const hamburger = document.getElementById('hamburger');
+// const navLinks = document.getElementById('nav-links');
+
+// hamburger.addEventListener('click', () => {
+//     navLinks.classList.toggle('display');
+//     //alert('This is the alert new!')
+// });
+
+// function myFunction() {
+  
+//     var x = document.querySelector('.nav-links');
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//      x.style.display = "block";
+//     }
+//  }
